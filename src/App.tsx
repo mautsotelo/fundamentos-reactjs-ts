afc6751ecf5843b6fc01734733b5e861b56359ec
Mauricio Sotelo <mautsotelo@gmail.com>
2023-06-09 20:16:40 -0300

    feat: inital commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { IPost, Post } from "./components/Post";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+const posts: IPost[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/mautsotelo.png',
+      name: 'Maurício Sotelo',
+      role: 'Frontend Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-06-07 21:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-06-07 22:00:00'),
+  },
+];
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                post={post}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Comment.tsx
@@ -0,0 +1,54 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({content, onDeleteComment}: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleLikeCount() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/mautsotelo.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Maurício sotelo</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeCount}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
